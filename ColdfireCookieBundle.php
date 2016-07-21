e836a69b0388ce1cddb6d3a4e8ccc3a701bde652
Mawunyo Local <mawunyo@codeagesolutions.co.uk>
2016-07-21 22:40:19 +0100

    initial commit

---
+++ ColdfireCookieBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Coldfire\CookieBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ColdfireCookieBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Coldfire\CookieBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ColdfireCookieBundle:Default:index.html.twig');
+    }
+}
